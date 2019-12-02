817a550b0c3363f43b9b4f7e6935bfcaad84ebc4
Leo Zhang <leo@fossa.io>
2019-12-02 11:23:23 -0800

    feat: Implement day 2 part 2

--- src/advent2019/day_02.clj
+++ src/advent2019/day_02.clj
@@ -25,10 +25,38 @@
         state
         (recur state' pc')))))
 
+(defn interpret-program [noun verb program]
+  (let [program' (assoc program 1 noun)
+        program'' (assoc program' 2 verb)]
+    (first (interpret program''))))
+
+(defn int-pairs
+  ([] (int-pairs 0))
+  ([n]
+   (lazy-seq (concat
+    (apply concat (map (fn [x] (map #(vector x %) (range n))) (range n)))
+    (int-pairs (inc n))))))
+
+(defn inputs-for [output program]
+  (first
+   (filter
+    #(= (% :output) output)
+    (map (fn [[noun verb]]
+           {:noun noun
+            :verb verb
+            :output (interpret-program noun verb program)})
+         (int-pairs)))))
+
+(defn load-program! [file]
+  (vec (map #(Integer/parseInt %)
+            (clojure.string/split (slurp file) #","))))
+
 (defn solve! [file]
-  (let [program (vec (map #(Integer/parseInt %)
-                          (clojure.string/split (slurp file) #",")))
-        program' (assoc program 1 12)
-        program'' (assoc program' 2 2)]
-    (println "Value at position 0:"
-             (first (interpret program'')))))
+  (let [program (load-program! file)
+        output (interpret-program 12 02 program)
+        {:keys [noun verb]} (inputs-for 19690720 program)]
+    (println "Value at position 0:" output)
+    (println "Inputs for 19690720:"
+             "noun:" noun
+             "verb:" verb)
+    (println "100 * noun + verb:" (+ verb (* noun 100)))))
