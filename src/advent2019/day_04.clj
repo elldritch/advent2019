7e340f427cc95871286f3bfeaa4861068d6e1b29
Leo Zhang <leo@leozhang.me>
2019-12-04 02:25:26 -0800

    feat: Implement day 4 part 2

--- src/advent2019/day_04.clj
+++ src/advent2019/day_04.clj
@@ -9,28 +9,49 @@
     []))
 
 (defn two-adjacent-digits-same? [n]
-  (:ok (reduce (fn [last d]
-                 (if (or (nil? last) (not= last d))
-                   d
-                   (reduced {:ok true})))
-               nil (digits n))))
+  (:ok (reduce (fn [last d] (if (= last d) (reduced {:ok true}) d))
+               nil
+               (digits n))))
 
 (defn non-decreasing-digits? [n]
-  (:ok (reduce (fn [{:keys [last ok]} d]
-                 (if (or (nil? last) (<= last d))
-                   {:last d :ok ok}
-                   (reduced {:ok false})))
-               {:last nil :ok true} (digits n))))
+  (not (:fail (reduce (fn [last d] (if (or (nil? last) (<= last d)) d (reduced {:fail true})))
+                      nil
+                      (digits n)))))
 
 (defn meets-criteria? [n]
   (and
    (two-adjacent-digits-same? n)
    (non-decreasing-digits? n)))
 
-(defn password-candidates-within-range [low high]
-  (filter meets-criteria? (range low (inc high))))
+(defn filter-inclusive-range [f low high]
+  (filter f (range low (inc high))))
+
+(def password-candidates-within-range
+  (partial filter-inclusive-range meets-criteria?))
+
+(defn two-adjacent-digits-same-not-part-of-larger-run? [n]
+  (let [result (reduce (fn [{:keys [last run]} d]
+                         (if (not= last d)
+                           (if (= run 2)
+                             (reduced {:ok true})
+                             {:last d :run 1})
+                           {:last d :run (inc run)}))
+                       {:last nil :run 0} (digits n))]
+    (or (:ok result) (= (result :run) 2))))
+
+(defn meets-extra-criteria? [n]
+  (and
+   (meets-criteria? n)
+   (two-adjacent-digits-same-not-part-of-larger-run? n)))
+
+(def stricter-password-candidates-within-range
+  (partial filter-inclusive-range meets-extra-criteria?))
 
 (defn solve! [file]
-  (let [passwords (apply password-candidates-within-range (parse (slurp file)))]
+  (let [input-range (parse (slurp file))
+        passwords (apply password-candidates-within-range input-range)
+        stricter-passwords (apply stricter-password-candidates-within-range input-range)]
     (println "Password examples:" (take 5 passwords))
-    (println "Password count:" (count passwords))))
+    (println "Password count:" (count passwords))
+    (println "Stricter password examples:" (take 5 stricter-passwords))
+    (println "Stricter password count:" (count stricter-passwords))))
--- test/advent2019/day_04_test.clj
+++ test/advent2019/day_04_test.clj
@@ -1,9 +1,13 @@
 (ns advent2019.day-04-test
   (:require [clojure.test :refer [deftest is testing]]
-            [advent2019.day-04 :refer [meets-criteria?]]))
+            [advent2019.day-04 :refer [meets-criteria? meets-extra-criteria?]]))
 
 (deftest password-guessing
   (testing "passwords that meet all criteria"
     (is (meets-criteria? 111111))
     (is (not (meets-criteria? 223450)))
-    (is (not (meets-criteria? 123789)))))
+    (is (not (meets-criteria? 123789))))
+  (testing "passwords meet stricter criteria"
+    (is (meets-extra-criteria? 112233))
+    (is (not (meets-extra-criteria? 123444)))
+    (is (meets-extra-criteria? 111122))))
