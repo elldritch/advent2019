a6da9f1ecdd7d89e714a499c314b0072042dab35
Leo Zhang <leo@leozhang.me>
2019-12-04 00:48:53 -0800

    feat: Implement day 3 part 2

--- src/advent2019/day_03.clj
+++ src/advent2019/day_03.clj
@@ -2,103 +2,99 @@
   (:require [clojure.math.numeric-tower :refer [abs]]))
 
 (defn parse [input]
-  (map
-   #(map (fn [[direction & distance]]
-           {:direction (case direction
-                         \L :left
-                         \R :right
-                         \U :up
-                         \D :down)
-            :distance (Integer/parseInt (apply str distance))}) %)
-   (map #(clojure.string/split % #",")
-        (clojure.string/split-lines input))))
+  (map #(map (fn [[direction & length]]
+               {:direction (case direction \L :left \R :right \U :up \D :down)
+                :length (Integer/parseInt (apply str length))}) %)
+       (map #(clojure.string/split % #",") (clojure.string/split-lines input))))
 
-(defn move [[x y] {:keys [direction distance]}]
+(defn move [[x y] {:keys [direction length]}]
   (case direction
-    :up [x (+ y distance)]
-    :down [x (- y distance)]
-    :right [(+ x distance) y]
-    :left [(- x distance) y]))
+    :up [x (+ y length)]
+    :down [x (- y length)]
+    :right [(+ x length) y]
+    :left [(- x length) y]))
 
-(defn reduce-directions [{lines :lines
-                          [x y] :position}
-                         {:keys [direction] :as step}]
-  (let [[x' y' :as next-position] (move [x y] step)]
-    (case direction
-      :up {:position next-position
-           :lines (cons {:vertical true :x x :bottom y :top y'} lines)}
-      :down {:position next-position
-             :lines (cons {:vertical true :x x
-                           :bottom y' :top y}
-                          lines)}
-      :right {:position next-position
-              :lines (cons {:horizontal true :y y
-                            :left x :right x'}
-                           lines)}
-      :left {:position next-position
-             :lines (cons {:horizontal true :y y
-                           :left x' :right x}
-                          lines)})))
+(defn acc-path-to-line [{lines :lines
+                         [x y] :position
+                         steps :steps}
+                        {:keys [direction length] :as path}]
+  (let [[x' y'] (move [x y] path)]
+    {:position [x' y']
+     :steps (+ steps length)
+     :lines (case direction
+              :up (cons {:vertical true :x x
+                         :bottom y :top y'
+                         :direction :up :steps steps} lines)
+              :down (cons {:vertical true :x x
+                           :bottom y' :top y
+                           :direction :down :steps steps} lines)
+              :right (cons {:horizontal true :y y
+                            :left x :right x'
+                            :direction :right :steps steps} lines)
+              :left (cons {:horizontal true :y y
+                           :left x' :right x
+                           :direction :left :steps steps} lines))}))
 
-(defn directions-to-lines [directions]
-  ((reduce reduce-directions {:lines () :position [0 0]} directions) :lines))
+(defn paths-to-lines [paths]
+  ((reduce acc-path-to-line {:lines () :position [0 0] :steps 0} paths) :lines))
 
-(defn reduce-intersections [{lines :lines
-                             [x y] :position
-                             intersections :intersections}
-                            {:keys [direction] :as step}]
-  (let [[x' y' :as next-position] (move [x y] step)]
-    (case direction
-      :up {:lines lines
-           :position next-position
-           :intersections (concat
-                           intersections
-                           (->> lines
-                                (filter :horizontal)
-                                (filter (fn [line] (and (< (line :left) x) (> (line :right) x))))
-                                (filter (fn [line] (and (> (line :y) y) (< (line :y) y'))))
-                                (map (fn [line] {:x x :y (line :y)}))))}
-      :down {:lines lines
-             :position next-position
-             :intersections (concat
-                             intersections
-                             (->> lines
-                                  (filter :horizontal)
-                                  (filter (fn [line] (and (< (line :left) x) (> (line :right) x))))
-                                  (filter (fn [line] (and (> (line :y) y') (< (line :y) y))))
-                                  (map (fn [line] {:x x :y (line :y)}))))}
-      :right {:lines lines
-              :position next-position
-              :intersections (concat
-                              intersections
-                              (->> lines
-                                   (filter :vertical)
-                                   (filter (fn [line] (and (< (line :bottom) y) (> (line :top) y))))
-                                   (filter (fn [line] (and (> (line :x) x) (< (line :x) x'))))
-                                   (map (fn [line] {:x (line :x) :y y}))))}
-      :left {:lines lines
-             :position next-position
-             :intersections (concat
-                             intersections
-                             (->> lines
-                                  (filter :vertical)
-                                  (filter (fn [line] (and (< (line :bottom) y) (> (line :top) y))))
-                                  (filter (fn [line] (and (> (line :x) x') (< (line :x) x))))
-                                  (map (fn [line] {:x (line :x) :y y}))))})))
+(defn between [low x high] (or (and (< low x) (< x high))
+                               (and (< high x) (< x low))))
 
-(defn directions-to-line-intersections [lines directions]
-  ((reduce reduce-intersections {:lines lines :position [0 0] :intersections ()} directions) :intersections))
+(defn distance [a b] (abs (- a b)))
+
+(defn steps-to-intersection-of-line-from-position [{:keys [direction steps] :as line} [x y]]
+  (case direction
+    :up (+ steps (distance y (line :bottom)) (distance x (line :x)))
+    :down (+ steps (distance y (line :top)) (distance x (line :x)))
+    :right (+ steps (distance x (line :left)) (distance y (line :y)))
+    :left (+ steps (distance x (line :right)) (distance y (line :y)))))
+
+(defn line-intersects-path-from-position? [line {:keys [direction] :as path} [x y]]
+  (let [[x' y'] (move [x y] path)]
+    (cond
+      (or (= direction :up)
+          (= direction :down)) (and (line :horizontal)
+                                      (between (line :left) x (line :right))
+                                      (between y (line :y) y'))
+      (or (= direction :right)
+          (= direction :left)) (and (line :vertical)
+                                      (between (line :bottom) y (line :top))
+                                      (between x (line :x) x')))))
+
+(defn line-intersection-with-path-from-position [line {:keys [direction]} [x y] path-steps]
+  (cond
+    (or (= direction :up)
+        (= direction :down)) {:x x :y (line :y)
+                                :steps (+ path-steps (steps-to-intersection-of-line-from-position line [x y]))}
+    (or (= direction :right)
+        (= direction :left)) {:x (line :x) :y y
+                                :steps (+ path-steps (steps-to-intersection-of-line-from-position line [x y]))}))
+
+(defn acc-intersections-of-path [{:keys [lines position steps intersections]} path]
+  {:lines lines
+   :position (move position path)
+   :steps (+ steps (path :length))
+   :intersections (concat intersections
+                          (->> lines
+                               (filter #(line-intersects-path-from-position? % path position))
+                               (map #(line-intersection-with-path-from-position % path position steps))))})
+
+(defn intersections-of-lines-and-paths [lines paths]
+  ((reduce acc-intersections-of-path {:lines lines :position [0 0] :steps 0 :intersections ()} paths) :intersections))
 
 (defn manhattan [{:keys [x y]}] (+ (abs x) (abs y)))
 
-(defn closest-intersection [wire-1 wire-2]
-  (let [wire-1-lines (directions-to-lines wire-1)
-        intersections (directions-to-line-intersections wire-1-lines wire-2)]
-    (apply min-key manhattan intersections)))
+(defn min-intersection [f wire-1 wire-2]
+  (let [wire-1-lines (paths-to-lines wire-1)
+        intersections (intersections-of-lines-and-paths wire-1-lines wire-2)]
+    (apply min-key f intersections)))
 
 (defn load-file! [file] (parse (slurp file)))
 
 (defn solve! [file]
   (let [[wire-1 wire-2] (load-file! file)
-        closest (closest-intersection wire-1 wire-2)]
-    (println "Closest intersection:" closest "Distance:" (manhattan closest))))
+        closest (min-intersection manhattan wire-1 wire-2)
+        fewest-steps (min-intersection :steps wire-1 wire-2)]
+    (println "Closest intersection:" closest "Distance:" (manhattan closest))
+    (println "Fewest step intersection:" fewest-steps "Steps:" (:steps fewest-steps))))
--- test/advent2019/day_03_test.clj
+++ test/advent2019/day_03_test.clj
@@ -1,15 +1,25 @@
 (ns advent2019.day-03-test
   (:require [clojure.test :refer [deftest is testing]]
-            [advent2019.day-03 :refer [parse closest-intersection manhattan]]))
+            [advent2019.day-03 :refer [parse min-intersection manhattan]]))
+
+(def panel-1 "R8,U5,L5,D3\nU7,R6,D4,L4")
+
+(def panel-2 "R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83")
+
+(def panel-3 "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7")
 
 (deftest intersections
   (testing "closest intersection"
-    (let [[wire-1 wire-2] (parse "R8,U5,L5,D3\nU7,R6,D4,L4")
-          closest (closest-intersection wire-1 wire-2)]
-      (is (= (manhattan closest) 6)))
-    (let [[wire-1 wire-2] (parse "R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83")
-          closest (closest-intersection wire-1 wire-2)]
-      (is (= (manhattan closest) 159)))
-    (let [[wire-1 wire-2] (parse "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7")
-          closest (closest-intersection wire-1 wire-2)]
-      (is (= (manhattan closest) 135)))))
+    (let [[wire-1 wire-2] (parse panel-1)]
+      (is (= (manhattan (min-intersection manhattan wire-1 wire-2)) 6)))
+    (let [[wire-1 wire-2] (parse panel-2)]
+      (is (= (manhattan (min-intersection manhattan wire-1 wire-2)) 159)))
+    (let [[wire-1 wire-2] (parse panel-3)]
+      (is (= (manhattan (min-intersection manhattan wire-1 wire-2)) 135))))
+  (testing "intersection with fewest steps"
+    (let [[wire-1 wire-2] (parse panel-1)]
+      (is (= (:steps (min-intersection :steps wire-1 wire-2)) 30)))
+    (let [[wire-1 wire-2] (parse panel-2)]
+      (is (= (:steps (min-intersection :steps wire-1 wire-2)) 610)))
+    (let [[wire-1 wire-2] (parse panel-3)]
+      (is (= (:steps (min-intersection :steps wire-1 wire-2)) 410)))))
