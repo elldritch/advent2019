e67c850b7b31692b44b8550d568fc46d7c756866
Leo Zhang <leo@leozhang.me>
2019-12-04 23:43:12 -0800

    test: Refactor intcode interpreter test runner and add d5p1 cases

--- test/advent2019/lib/intcode_test.clj
+++ test/advent2019/lib/intcode_test.clj
@@ -2,15 +2,23 @@
   (:require [clojure.test :refer [deftest is testing]]
             [advent2019.lib.intcode :refer [run-program]]))
 
+(defn run [state expected]
+  (is (= (:state (run-program state nil))) expected))
+
 (deftest intcode
   (testing "addition and multiplication instructions"
-    (is (= (:state (run-program [1 9 10 3 2 3 11 0 99 30 40 50] nil))
-           [3500 9 10 70 2 3 11 0 99 30 40 50]))
-    (is (= (:state (run-program [1 0 0 0 99] nil))
-           [2 0 0 0 99]))
-    (is (= (:state (run-program [2 3 0 3 99] nil))
-           [2 3 0 6 99]))
-    (is (= (:state (run-program [2 4 4 5 99 0] nil))
-           [2 4 4 5 99 9801]))
-    (is (= (:state (run-program [1 1 1 4 99 5 6 0 99] nil))
-           [30 1 1 4 2 5 6 0 99]))))
+    (run [1 9 10 3 2 3 11 0 99 30 40 50]
+         [3500 9 10 70 2 3 11 0 99 30 40 50])
+    (run [1 0 0 0 99]
+         [2 0 0 0 99])
+    (run [2 3 0 3 99]
+         [2 3 0 6 99])
+    (run [2 4 4 5 99 0]
+         [2 4 4 5 99 9801])
+    (run [1 1 1 4 99 5 6 0 99]
+         [30 1 1 4 2 5 6 0 99]))
+  (testing "parameter modes"
+    (run [1002 4 3 4 33]
+         [1002 4 3 4 99])
+    (run [1101 100 -1 4 0]
+         [1101 100 -1 4 99])))
