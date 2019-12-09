2d1ecdb5476eb1c8b3700e6a99ef0a10e0d21fdf
Leo Zhang <leo@leozhang.me>
2019-12-08 22:36:29 -0800

    feat: Implement day 9 part 2
    - Add day 9 solving to CI

--- src/advent2019/day_09.clj
+++ src/advent2019/day_09.clj
@@ -7,5 +7,7 @@
 (defn solve! [file]
   (let [program (load-program! file)
         continuation (run-program program)
-        test-mode (resume-program-with-input continuation 1)]
-    (println "BOOST test mode outputs:" (:outputs (gather-outputs test-mode)))))
+        test-mode (resume-program-with-input continuation 1)
+        boost-mode (resume-program-with-input continuation 2)]
+    (println "BOOST test mode outputs:" (:outputs (gather-outputs test-mode)))
+    (println "BOOST sensor boost mode outputs:" (:outputs (gather-outputs boost-mode)))))
