2018d2e069d2e9db86b44d529c248ba4db9febf8
Leo Zhang <leo@leozhang.me>
2019-12-17 23:10:54 -0800

    feat: Implement day 16 part 1

---
+++ src/advent2019/day_16.clj
@@ -0,0 +1,22 @@
+(ns advent2019.day-16)
+
+(def base-pattern [0 1 0 -1])
+
+(defn pattern [base position]
+  (rest (apply concat (repeat (mapcat #(repeat position %) base)))))
+
+(defn phase [digits]
+  (mapv (fn [position]
+         (let [output (reduce + (map * digits (pattern base-pattern position)))]
+           (mod (if (pos? output) output (- output)) 10)))
+       (range 1 (inc (count digits)))))
+
+(defn phases [digits phase-count] (nth (iterate phase digits) phase-count))
+
+(defn parse [input] (map #(Character/digit % 10) input))
+
+(defn load-file! [file] (parse (slurp file)))
+
+(defn solve! [file]
+  (let [digits (load-file! file)]
+    (println "First 8 digits after 100 phases of FFT:" (take 8 (phases digits 100)))))
